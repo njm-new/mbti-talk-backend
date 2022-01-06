8a78923f98de99a697203bcce38c54a2c836b07c
mono404 <kwh6543@hanmail.net>
2022-01-06 22:43:51 +0900

    feat: Comment API 구현
    댓글에 대한 CRUD mothod 구현
    
    Issues: #20

---
+++ src/main/java/com/mbtitalkbackend/comment/mapper/CommentMapper.java
@@ -0,0 +1,29 @@
+package com.mbtitalkbackend.comment.mapper;
+
+import com.mbtitalkbackend.comment.model.entity.CommentEntity;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    @Insert("INSERT INTO comment (postId, memberId, title, content, likeCount) VALUES(#{postId}, #{memberId}, #{title}, #{content}, #{likeCount})")
+    @Options(useGeneratedKeys = true, keyProperty = "commentId")
+    Integer createComment(CommentEntity commentEntity);
+
+    @Select("SELECT * FROM comment WHERE commentId = #{commentId}")
+    CommentEntity findCommentByCommentId(long commentId);
+
+    @Select("SELECT * FROM comment WHERE postId = #{postId}")
+    List<CommentEntity> findCommentList(long postId);
+
+    @Update("UPDATE comment SET content = #{content} WHERE commentId = #{commentId}")
+    Integer updateComment(CommentEntity commentEntity);
+
+    @Delete("DELETE FROM comment WHERE commentId = #{commentId}")
+    Integer deleteCommentByCommentId(long commentId);
+
+    @Update("UPDATE comment SET modifiedTime = now() WHERE commentId = #{commentId}")
+    Integer updateCommentModifiedTime(long commentId);
+}
---
+++ src/main/java/com/mbtitalkbackend/comment/service/CommentService.java
@@ -0,0 +1,72 @@
+package com.mbtitalkbackend.comment.service;
+
+import com.mbtitalkbackend.comment.mapper.CommentMapper;
+import com.mbtitalkbackend.comment.model.VO.CommentListVO;
+import com.mbtitalkbackend.comment.model.VO.CommentVO;
+import com.mbtitalkbackend.comment.model.entity.CommentEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+
+    private final CommentMapper commentMapper;
+
+    public Integer createComment(CommentVO commentVO) {
+
+        CommentEntity commentEntity = CommentEntity.create(commentVO);
+
+        return commentMapper.createComment(commentEntity);
+    }
+
+    public CommentVO findCommentByCommentId(long commentId) {
+
+        try {
+            CommentEntity commentEntity = commentMapper.findCommentByCommentId(commentId);
+
+            return CommentVO.of(commentEntity);
+        }
+        catch (NullPointerException e) {
+            throw new NullPointerException();
+        }
+    }
+
+    public CommentListVO findCommentList(long postId) {
+
+        List<CommentEntity> commentEntityList = commentMapper.findCommentList(postId);
+        List<CommentVO> commentVOList = new ArrayList<>();
+
+        for (CommentEntity commentEntity : commentEntityList) {
+            commentVOList.add(CommentVO.of(commentEntity));
+        }
+
+        return CommentListVO.create(commentVOList);
+    }
+
+    public Integer updateComment(long commentId, CommentVO commentVO) {
+
+        try {
+            commentMapper.updateCommentModifiedTime(commentId);
+            CommentEntity commentEntity = commentMapper.findCommentByCommentId(commentId);
+
+//            commentEntity.setMemberId(commentVO.getMemberId());
+            commentEntity.setContent(commentVO.getContent());
+
+            return commentMapper.updateComment(commentEntity);
+        }
+        catch (NullPointerException e) {
+            throw new NullPointerException();
+        }
+
+    }
+
+    public Integer deleteCommentByCommentId(long commentId) {
+
+        return commentMapper.deleteCommentByCommentId(commentId);
+
+    }
+}
