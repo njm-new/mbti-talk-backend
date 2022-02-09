d325e0eeea57678ac3586fc795f0de0f8ff957ff
mono404 <kwh6543@hanmail.net>
2022-02-09 16:31:42 +0900

    hotfix: board 관련 쿼리문 수정
    기존엔 전체 기준 12개 뽑고 그 후 boardId 비교
    정상적으로 수정
    
    Issues: X

--- src/main/java/com/mbtitalkbackend/board/mapper/BoardMapper.java
+++ src/main/java/com/mbtitalkbackend/board/mapper/BoardMapper.java
@@ -13,13 +13,13 @@ public interface BoardMapper {
     @Select("SELECT * FROM post WHERE postId > 0")
     List<PostEntity> findAllPosts();
 
-    @Select("SELECT * FROM post BETWEEN #{pageNum} * #{rowPerPage} AND (#{pageNum} + 1) * #{rowPerPage}")
+    @Select("SELECT R1.* FROM (SELECT * FROM post ORDER BY postId DESC ) R1 LIMIT #{pagingCriteria.pageNum}, #{pagingCriteria.rowPerPage}")
     List<PostEntity> findAllPostsWithPaging(PagingCriteria pagingCriteria);
 
-    @Select("SELECT * FROM post WHERE boardId = #{boardId} AND postId BETWEEN #{pagingCriteria.pageNum} * #{pagingCriteria.rowPerPage} AND (#{pagingCriteria.pageNum} + 1) * #{pagingCriteria.rowPerPage}")
+    @Select("SELECT R1.* FROM (SELECT * FROM post WHERE boardId = #{boardId} ORDER BY postId DESC ) R1 LIMIT #{pagingCriteria.pageNum}, #{pagingCriteria.rowPerPage}")
     List<PostEntity> findAllPostsWithBoardId(PagingCriteria pagingCriteria, String boardId);
 
-    @Select("SELECT * FROM post WHERE memberId = #{memberId} AND postId BETWEEN #{pagingCriteria.pageNum} * #{pagingCriteria.rowPerPage} AND (#{pagingCriteria.pageNum} + 1) * #{pagingCriteria.rowPerPage}")
+    @Select("SELECT R1.* FROM (SELECT * FROM post WHERE memberId = #{memberId} ORDER BY postId DESC ) R1 LIMIT #{pagingCriteria.pageNum}, #{pagingCriteria.rowPerPage}")
     List<PostEntity> findMyPosts(PagingCriteria pagingCriteria, String memberId);
 
     @Select("SELECT R1.* FROM (SELECT * FROM post WHERE boardId = #{boardId} ORDER BY ((likeCount * 10) + viewCount) DESC) R1 LIMIT #{pagingCriteria.pageNum}, #{pagingCriteria.rowPerPage}")
