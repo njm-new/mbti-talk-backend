45f79edfd510ac39d86592b04ab4630c73d1569f
mono404 <kwh6543@hanmail.net>
2022-02-14 23:20:17 +0900

    chore: 빌드 순서 변경
    빌드 문제로 인한 순서 변경
    
    Issues: X

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,14 @@ git_token="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"
 # move working directory
 # cd /home/centos/mbti-talk-backend
 
+# remove container
+echo "=> Remove previous container..."
+docker rm -f ${container_name}
+
+# remove image
+echo "=> Remove previous image..."
+docker rmi -f ${docker_username}/${image_name}:${version}
+
 # git pull
 echo "=> Git pull origin..."
 git pull "https://${git_username}:${git_token}@github.com/njm-new/mbti-talk-backend.git" develop
@@ -18,14 +26,6 @@ echo "=> Gradle clean build..."
 ./gradlew clean build -x test
 
 echo "## Automation docker-database build and run ##"
-
-# remove container
-echo "=> Remove previous container..."
-docker rm -f ${container_name}
-
-# remove image
-echo "=> Remove previous image..."
-docker rmi -f ${docker_username}/${image_name}:${version}
 
 # new-build/re-build docker image
 echo "=> Build new image..."
